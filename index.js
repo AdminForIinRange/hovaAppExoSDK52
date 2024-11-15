0029d91ec33f779c66106f27682870eb69ef51ad
Anjesh <bhattaraianjesh123@gmail.com>
2024-11-15 12:32:29 +1030

    commit 1 (stable tail)

---
+++ index.js
@@ -0,0 +1,8 @@
+// index.js
+
+import { registerRootComponent } from 'expo';
+import "./global.css";
+import App from './src/app';
+
+
+registerRootComponent(App);
---
+++ src/app/_layout.jsx
@@ -0,0 +1,16 @@
+
+// app/_layout.js
+import { Stack } from 'expo-router';
+import "../../global.css";
+
+
+export default function RootLayout() {
+
+
+  return (
+    <Stack>
+      <Stack.Screen name="index"  />
+      
+    </Stack>
+  );
+}
